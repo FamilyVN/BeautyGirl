7d16766eb97312edab1e41be785d9093c71d6e4f
TuanAnh <vtabk2@gmail.com>
2018-05-08 17:44:13 +0700

    ref home activity

---
+++ app/src/main/java/com/tuananh/family/beautygirl/view/adapter/HomeAdapter.java
@@ -0,0 +1,59 @@
+package com.tuananh.family.beautygirl.view.adapter;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.tuananh.family.beautygirl.R;
+import com.tuananh.family.beautygirl.view.fragment.home.FollowingFragment;
+import com.tuananh.family.beautygirl.view.fragment.home.TrendingFragment;
+
+/**
+ * Created by FRAMGIA\vu.tuan.anh on 08/05/2018.
+ */
+
+public class HomeAdapter extends FragmentStatePagerAdapter {
+    private static final int COUNT = 2;
+    private static final int FOLLOWING = 1;
+    private static final int TRENDING = 2;
+    private Context mContext;
+
+    public HomeAdapter(Context context, FragmentManager fragmentManager) {
+        super(fragmentManager);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case FOLLOWING:
+                return FollowingFragment.newInstance();
+            case TRENDING:
+            default:
+                return TrendingFragment.newInstance();
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        int resTitle;
+        switch (position) {
+            case FOLLOWING:
+                resTitle = R.string.text_tab_following;
+                break;
+            case TRENDING:
+            default:
+                resTitle = R.string.text_tab_trending;
+                break;
+        }
+        return mContext.getResources().getString(resTitle);
+    }
+
+    @Override
+    public int getCount() {
+        return COUNT;
+    }
+}
---
+++ app/src/main/java/com/tuananh/family/beautygirl/view/fragment/home/FollowingFragment.java
@@ -0,0 +1,14 @@
+package com.tuananh.family.beautygirl.view.fragment.home;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by FRAMGIA\vu.tuan.anh on 08/05/2018.
+ */
+
+public class FollowingFragment extends Fragment {
+    public static FollowingFragment newInstance() {
+        return new FollowingFragment();
+    }
+}
