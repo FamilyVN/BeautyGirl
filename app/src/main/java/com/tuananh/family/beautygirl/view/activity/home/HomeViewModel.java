e4f372a562f784c8f584dc52c1746b8ea0cea165
TuanAnh <vtabk2@gmail.com>
2018-05-10 18:49:27 +0700

    login

---
+++ app/src/main/java/com/tuananh/family/beautygirl/view/activity/home/HomeViewModel.java
@@ -0,0 +1,12 @@
+package com.tuananh.family.beautygirl.view.activity.home;
+
+import com.tuananh.family.beautygirl.databinding.ActivityHomeBinding;
+
+/**
+ * Created by FRAMGIA\vu.tuan.anh on 10/05/2018.
+ */
+
+public class HomeViewModel {
+    public HomeViewModel(ActivityHomeBinding binding) {
+    }
+}
---
+++ app/src/main/java/com/tuananh/family/beautygirl/view/activity/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.tuananh.family.beautygirl.view.activity.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tuananh.family.beautygirl.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
